95c5fc2e5c1bf3740b68681f81e953c8c2351e23
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-05-05 05:23:18 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -21,7 +21,7 @@ gen_key = InlineKeyboardMarkup(
             InlineKeyboardButton(text="Aɴɪᴍᴇ 1", url=f"https://t.me/Dark_Support_Group"),
             InlineKeyboardButton(text="Aɴɪᴍᴇ 2", url=f"https://t.me/Dark_Support_Group"),
         ],
-        [InlineKeyboardButton(text="Jᴜɪᴜᴛsᴜ ᴋᴀɪsᴇɴ", url=f"https://t.me/Jujustu_Kaisen_Season_2_Hindi")],
+        [InlineKeyboardButton(text="VINCENZO", url=f"https://telegram.me/PublicFileStore01_Bot?start=PUBLIC_NjY1MQ==")],
     ]
 )
 
