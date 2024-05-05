1d31669ffc422916cd51b41daac6652765526011
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-05-05 14:08:02 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -1,37 +1,30 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Define the command handler for the /start command
-def start(update, context):
-    reply_markup = create_inline_keyboard()
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your bot.", reply_markup=reply_markup)
+from config import SUPPORT_CHAT
 
-# Define the callback handler for inline keyboard button clicks
-def button_callback(update, context):
-    query = update.callback_query
-    query.answer()
 
-    if query.data == 'next':
-        context.bot.send_message(chat_id=query.message.chat_id, text="Next button clicked!")
-    elif query.data == 'previous':
-        context.bot.send_message(chat_id=query.message.chat_id, text="Previous button clicked!")
-
-# Create the inline keyboard
-def create_inline_keyboard():
-    keyboard = [
-        [InlineKeyboardButton("Previous", callback_data='previous'),
-         InlineKeyboardButton("Next", callback_data='next')]
+keyboard = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+            ),
+        ],
     ]
-    return InlineKeyboardMarkup(keyboard)
+)
 
-# Create an Updater and attach the handlers
-updater = Updater(token='6571094339:AAGcENp3AmPPJslc88ERGsT_VdJ20hKTtvQ', use_context=True)
-dispatcher = updater.dispatcher
+gen_key = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram"),
+        ],
+        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
+    ]
+)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-button_handler = CallbackQueryHandler(button_callback)
-dispatcher.add_handler(button_handler)
-
-# Start the bot
-updater.start_polling()
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+)
